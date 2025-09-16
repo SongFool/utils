958651f71755681b3dd64930cb33d141b7dcbad3
SongFool <102405752+SongFool@users.noreply.github.com>
2025-09-16 15:45:50 +0800

    Update main.c

--- Vector/main.c
+++ Vector/main.c
@@ -1,1 +1,20 @@
+#include "vector.h"
+int main() {
 
+    vector vec;
+    vector_init(&vec, sizeof(int));
+
+    // 提前申请 100 个空间，避免频繁 realloc
+
+    for (int i = 0; i < 10; i++) {
+        vector_push_back(&vec, &i);
+    }
+
+    for(int i = 0; i < vector_size(&vec); i ++){
+        printf("data : %d\r\n",((int*) vec.data)[i]);
+    }
+    printf("size = %zu, capacity = %zu\n", vector_size(&vec), vector_capacity(&vec));
+
+    vector_free(&vec);
+    return 0;
+}
