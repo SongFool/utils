6055bb3d7bb545017007361bd53054e3392fff62
SongFool <102405752+SongFool@users.noreply.github.com>
2026-06-13 13:06:49 +0800

    Add queue_t structure and queue functions

--- audio_queue/queue.h
+++ audio_queue/queue.h
@@ -1,1 +1,11 @@
 
+typedef struct{
+    void **data;
+    int count;
+    int size;
+    int read;
+    int write;
+}queue_t;
+int queue_push(queue_t *q, void *data);
+void * queue_pop(queue_t *q);
+int queue_init(queue_t *q, int size,void **data);
